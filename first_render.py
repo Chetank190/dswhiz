00fb9eb43400d4c724efecfe22a1e44e21ca84fb
Chetank190 <chetan3471@gmail.com>
2022-07-24 12:54:44 -0400

    removing unnecessary files and folders

--- first_render.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    first_name = "Chetan"
-    return render_template('index.html',
-                           f_name = first_name)
