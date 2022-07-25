835c34b599cef7b6499b80e2ab7d95d8ba7e4034
Chetank190 <chetan3471@gmail.com>
2022-07-24 21:44:20 -0400

    add to app

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Create Index Page
 @app.route('/')
-def hello_world():
-    return "Hello World!, how are u? lol"
+def index():
+    first_name = "Chetan"
+    favorite_pizza = ["Pepperoni", "Cheese", "Supreme", "Mushroom"]
+    return render_template('index.html',
+                           f_name=first_name,
+                           favorite_pizza=favorite_pizza)
+
+    # Create About Page
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
